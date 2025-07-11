ec77166ced601272b9117820ca9b9be4ef02387b
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:24:19 +0530

    added the function for calculate the average rating of product

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product
+from review.models import Review
 
 class ProductSerializer(serializers.ModelSerializer):
     average_rating = serializers.SerializersMethodField()
@@ -9,4 +10,7 @@ class ProductSerializer(serializers.ModelSerializer):
         field = '__all__'
 
     def get_average_rating(self, obj):
-        pass
+        reviews = Review.objects.filter(product=obj)
+        if reviews.exists():
+            return round(sum(r.rating for r in reviews) / reviews.count(), 2)
+        return None
