9b69205c173776668504fb374532aae2feebf03f
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:17:52 +0530

    review/serializer added

---
+++ backend/review/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('user',)
+
+    def validate_rating(self, value):
+        if not (1 <= value <= 5):
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
