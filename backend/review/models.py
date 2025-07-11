b72d273e85660b3ad512a9219dce19de3ec5ec36
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:16:57 +0530

    review app added and review/models also added

--- backend/review/models.py
+++ backend/review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from product.models import Product
 
 # Create your models here.
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review_text = models.TextField()
+
+    class Meta:
+        unique_together = ['product','user']
+        
