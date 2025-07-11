e0b36385d6a028c9b24ecbd8262f405d04faab72
Sreeraj <psreeraj711@gmail.com>
2025-07-12 04:55:00 +0530

    users/serializers added

---
+++ backend/users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        field = ('username','password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+    
