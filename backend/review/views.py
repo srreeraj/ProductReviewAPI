c4c58955cdc4ba87588138fcf7b623d0f9161a16
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:18:32 +0530

    review/view added

--- backend/review/views.py
+++ backend/review/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+class ReviewCreateView(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ProductReviewList(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        product_id = self.kwargs['product_id']
+        return Review.objects.filter(product_id=product_id)
