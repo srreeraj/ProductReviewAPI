8dbe97c154b67950b3dea7dd8ab3801583bb257f
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:05:52 +0530

    settings updated and product app added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'users'
 ]
 
 MIDDLEWARE = [
