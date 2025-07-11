0a38dc53c37aa6ddfc3e5c629398f5f78e725bfb
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:10:05 +0530

    product/views added

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.persmissions import BasePermission, SAFE_METHODS
 # Create your views here.
+
+class IsAdminUserOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user and request.user.is_authenticated and request.user.is_staff
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
