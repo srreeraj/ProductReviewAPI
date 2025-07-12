d69d8c281773c53d691ea7cb92e38a543eb696ce
Sreeraj <psreeraj711@gmail.com>
2025-07-12 05:33:06 +0530

    backend core urls updated

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework.routers import DefualtRouter
+from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSet
 from review.views import ReviewCreateView, ProductReviewList
 
-router = DefualtRouter()
+router = DefaultRouter()
 router.register(r'products',ProductViewSet)
 
 urlpatterns = [
