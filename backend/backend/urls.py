0e81ae0c62e64bcc5f2ab6fd9c6ab326fb7f53d4
Sreeraj <psreeraj711@gmail.com>
2025-07-12 06:31:09 +0530

    updated due to drf-ysag package is installed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,7 +20,7 @@ from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSet
 from review.views import ReviewCreateView, ProductReviewList
 from rest_framework import permissions
-from drf_ysag.views import get_schema_view
+from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 router = DefaultRouter()
