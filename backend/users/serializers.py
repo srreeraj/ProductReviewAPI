9002a1cd72b32d8fb5878d66c6a2f320f7f935c3
Sreeraj <psreeraj711@gmail.com>
2025-07-12 06:39:58 +0530

    updated  updated due to typo error

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,7 +7,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        field = ('username','password')
+        fields = ('username','password')
 
     def create(self, validated_data):
         user = User.objects.create_user(
