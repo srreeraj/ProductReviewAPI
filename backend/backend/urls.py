1f14898025336fb6b7077e5e7b073b73ba79f096
Sreeraj <psreeraj711@gmail.com>
2025-07-12 06:21:07 +0530

    backend core urls updated for implement swagger ui

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,25 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from product.views import ProductViewSet
 from review.views import ReviewCreateView, ProductReviewList
+from rest_framework import permissions
+from drf_ysag.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'products',ProductViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Product Review API',
+        default_version='v1',
+        description='API for Products, Reviews and Authentication',
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email='support@example.com'),
+        license=openapi.License(name='BSD License')
+    ),
+    public=True,
+    permission_classes = [permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/',admin.site.urls),
@@ -29,4 +45,6 @@ urlpatterns = [
     path('api/',include(router.urls)),
     path('api/products/<int:product_id>/reviews/',ProductReviewList.as_view()),
     path('api/reviews/create/',ReviewCreateView.as_view()),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
